e8c8c86f1954e64327f415865bc0a4ccad0a2c26
pavan <admin@Q60>
2017-12-11 17:27:51 +0530

    new module

---
+++ Demo/src/Pack/Test3.java
@@ -0,0 +1,18 @@
+package Pack;
+
+import org.testng.annotations.Test;
+
+public class Test3 
+{
+	@Test
+	public void test()
+	{
+		System.out.println("test 1");
+	}
+	@Test
+	public void test2()
+	{
+		System.out.println("test 2");
+	}
+
+}
