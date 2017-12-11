8002144b57586371f07088a1957b531e3a7faa2d
pavan <admin@Q60>
2017-12-11 17:23:42 +0530

    new functionality

---
+++ Demo/src/Pack/Test2.java
@@ -0,0 +1,18 @@
+package Pack;
+
+import org.testng.annotations.Test;
+
+public class Test2 
+{
+	@Test
+	public void test3()
+	{
+		System.out.println("test 3");
+	}
+	@Test
+	public void test4()
+	{
+		System.out.println("test 4");
+	}
+
+}
